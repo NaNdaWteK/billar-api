390f57d85bf6ca0d2fdb459aee866afa484aeb08
NandawteK <nandawtek@gmail.com>
2023-02-04 02:34:21 +0100

    pass test before push deploy image

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-APP_VERSION=$(cat package.json | jq -r .version)
 git fetch
 git checkout production
 git pull origin production
-APP_VERSION=${APP_VERSION} docker-compose -f production.yml up --build
+APP_VERSION=$(cat package.json | jq -r .version) docker-compose -f production.yml up --build
