adbbaccb7ab3c6610ac42c19351f693569f95e7c
NandawteK <nandawtek@gmail.com>
2023-02-04 02:14:00 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 APP_VERSION=$(cat package.json | jq -r .version)
+git fetch
+git checkout production
+git pull origin production
 APP_VERSION=${APP_VERSION} docker-compose -f production.yml up --build
